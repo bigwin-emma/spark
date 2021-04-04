44e109b7e1acd892b463b9992874dc9a296c16f2
18575573773@163.com <Bigwin123456>
2021-04-04 09:51:42 +0800

    main方法添加

--- src/test/java/com/ibeifeng/sparkproject/AppTest.java
+++ src/test/java/com/ibeifeng/sparkproject/AppTest.java
@@ -38,5 +38,7 @@ public class AppTest
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
